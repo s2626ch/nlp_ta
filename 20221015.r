c20ee391139b9767771f8be3c457339d3f066db5
chihosong <chihosong@gmail.com>
2022-10-18 13:59:24 +0900

    10/15

--- 20221015.r
+++ 20221015.r
@@ -45,3 +45,81 @@ get_opinion(buzz$CONTENT[1], n = 3)
 
 ## 군산대학교 한국어감성사전 분석
 get_polarity(buzz$CONTENT[1])
+
+library(tidyverse)
+library(bitTA)
+library(wordcloud2)
+library(tidytext)
+
+
+polarity_word_counts <- buzz %>% 
+  filter(row_number() <= 200) %>% 
+  mutate(morpho_content = get_spacing(CONTENT)) %>%   
+  mutate(morpho_content = morpho_mecab(CONTENT, type = "morpheme") %>% 
+           purrr::map_chr(paste, collapse = " ") %>% 
+           stringr::str_remove_all("\"")) %>% 
+  tidytext::unnest_ngrams(
+    unigram,
+    morpho_content,
+    n = 1
+  ) %>% 
+  left_join(
+    sentiment_dic,
+    by = c("unigram" = "word")
+  ) %>% 
+  filter(!is.na(word_root)) %>% 
+  filter(!str_length(unigram) == 1) %>% 
+  mutate(polarity_class = ifelse(polarity > 0, "긍정", "부정")) %>% 
+  count(unigram, polarity_class, sort = TRUE) 
+
+polarity_word_counts
+
+polarity_word_counts %>%
+  filter(n > 2) %>%
+  mutate(n = ifelse(polarity_class == "부정", -n, n)) %>%
+  mutate(unigram = reorder(unigram, n)) %>%
+  ggplot(aes(unigram, n, fill = polarity_class)) +
+  geom_col() +
+  coord_flip() +
+  labs(y = "긍부정 극성의 발현 빈도")
+
+
+
+
+buzz %>% 
+  group_by(SECTION) %>% 
+  tally()
+
+term_noun <- buzz %>% 
+  filter(SECTION %in% "맘스홀릭") %>% 
+  filter(row_number() == 1) %>% 
+  select(CONTENT) %>% 
+  pull() %>% 
+  morpho_mecab()
+
+
+term_noun %>% 
+  table() 
+
+term_noun %>% 
+  table() %>% 
+  wordcloud2::wordcloud2(fontFamily = "NanumSquare")
+
+dic_stopwords <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "년", "월", "일", "때", "전")
+
+is_stopwords <- term_noun %in% dic_stopwords
+is_stopwords
+
+clean_term_noun <- term_noun[!is_stopwords]
+clean_term_noun
+
+clean_term_noun %>% 
+  table() %>% 
+  wordcloud2(fontFamily = "NanumSquare")
+
+buzz %>% 
+  filter(SECTION %in% "맘스홀릭") %>% 
+  filter(row_number() == 1) %>% 
+  select(CONTENT) %>% 
+  get_polarity()
+
